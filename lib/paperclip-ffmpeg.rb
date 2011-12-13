b678a9f03b0e4226e89074c1924b27279c2abc87
Steve Madsen <steve@lightyearsoftware.com>
2011-12-13 16:38:47 -0500

    Added :preset option and H.264 Baseline 3.0 settings.

--- lib/paperclip-ffmpeg.rb
+++ lib/paperclip-ffmpeg.rb
@@ -24,6 +24,17 @@ module Paperclip
           @convert_options[:output].reverse_merge! options[:convert_options][:output]
         end
       end
+      case options[:preset]
+      when :baseline30
+        @convert_options[:output].merge!({
+          :vcodec => 'libx264', :b => '512k', :flags => '+loop+mv4', :cmp => '256',
+          :partitions => '+parti4x4+parti8x8+partp4x4+partp8x8+partb8x8',
+          :me_method => 'hex', :subq => '7', :trellis => '1', :refs => '5', :bf => '0',
+          :flags2 => '+mixed_refs', :coder => '0', :me_range => '16', :g => '250',
+          :keyint_min => '25', :sc_threshold => '40', :i_qfactor => '0.71', :qmin => '10',
+          :qmax => '51', :qdiff => '4'
+        })
+      end
       
       @geometry        = options[:geometry]
       @file            = file
